89028eb18161b3099d1c7be21c4d4916e2dce110
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2023-01-18 13:08:17 +0000

    created form in dictionary

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Dictionary() {
-    return (
-      
+  let [keyword, setKeyword] = useState(null);
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for  ${keyword}`);
+  }
+
+  function handleWordSearch(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleWordSearch} />
+      </form>
+    </div>
   );
 }
