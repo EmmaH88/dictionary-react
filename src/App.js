634de9e1ee7d99cc4dd183ef1f5c8892d0931a6a
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2023-01-19 11:34:10 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="container">
+    <div className="container mt-3">
       <div className="App">
         <header className="App-header">
           <h1>🧾 Dictionary App 🧾</h1>
